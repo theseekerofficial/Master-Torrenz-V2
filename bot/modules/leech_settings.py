52d7bdebbadfb516f1c6e44c8495d4ac6e1be06c
The Seeker <110098713+theseeker99@users.noreply.github.com>
2022-10-30 10:57:35 +0530

    Update leech_settings.py

--- bot/modules/leech_settings.py
+++ bot/modules/leech_settings.py
@@ -38,7 +38,7 @@ def getleechinfo(from_user):
 
     button = buttons.build_menu(1)
 
-    text = f"<u>Leech Settings for <a href='tg://user?id={user_id}'>{name}</a></u>\n"\
+    text = f"<u>⚜️ Leech Settings for <a href='tg://user?id={user_id}'>{name}</a></u>\n"\
            f"Leech Type <b>{ltype}</b>\n"\
            f"Custom Thumbnail <b>{thumbmsg}</b>"
     return text, button
