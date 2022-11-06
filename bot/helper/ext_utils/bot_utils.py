b67ec082d29db767bbc613fa442f09256c11ca30
The Seeker <94602611+theseekerofficial@users.noreply.github.com>
2022-11-07 01:28:38 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -193,7 +193,7 @@ def get_readable_message():
                 elif 'M' in spd:
                     up_speed += float(spd.split('M')[0]) * 1048576
         bmsg = f"<b>Version 💙:</b> <code>Master Torrenz V5.12</code>"
-        bmsg = f"<b>CPU 🌀:</b> {cpu_percent()}% | <b>FREE ♻️:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
+        bmsg = f"<b>CPU :</b> {cpu_percent()}% | <b>FREE ♻️:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
         bmsg += f"\n<b>RAM 🔰:</b> {virtual_memory().percent}% | <b>UPTIME 👀:</b> {get_readable_time(time() - botStartTime)}"
         bmsg += f"\n<b>DL 🔽:</b> {get_readable_file_size(dl_speed)}/s | <b>UL 🔼:</b> {get_readable_file_size(up_speed)}/s"
         buttons = ButtonMaker()
