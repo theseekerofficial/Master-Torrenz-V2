8b0f1a58d912d3dad1c33e73674b32b02d9c3780
The Seeker <94602611+theseekerofficial@users.noreply.github.com>
2022-11-07 19:08:14 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -196,7 +196,7 @@ def get_readable_message():
         bmsg = f"<b>CPU 🌀:</b> {cpu_percent()}% | <b>FREE ♻️:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
         bmsg += f"\n<b>RAM 🔰:</b> {virtual_memory().percent}% | <b>UPTIME 👀:</b> {get_readable_time(time() - botStartTime)}"
         bmsg += f"\n<b>DL 🔽:</b> {get_readable_file_size(dl_speed)}/s | <b>UL 🔼:</b> {get_readable_file_size(up_speed)}/s"
-        bmsg += f"\n<b>Version 💙:</b> <code>Master Torrenz V5.12</code>"
+        bmsg += f"\n<b>Version 💙:</b> <code>Master Torrenz V3.12</code>"
         buttons = ButtonMaker()
         buttons.sbutton("🔻 Statistics 🔻", str(FOUR))
         sbutton = buttons.build_menu(1)
